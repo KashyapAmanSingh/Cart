57adb3fa769e107efd1c7d8b45502fa710f73245
kashyapAmansingh <coder85113@gmail.com>
2023-09-28 22:58:22 +0530

    Update-Integerate with reducers slice

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,12 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./Slice";  
 import productSlice from "./ProductSlice";
 import FilterSortSlice from "./FilterSortSlice";
-
+import reviewsSlice from "./ReviewSlice";
+import userSlice from "./UserInfoSlice";
+ 
 const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
     Product:productSlice.reducer,
     FilterSortSlice:FilterSortSlice.reducer,
+    Reviews: reviewsSlice.reducer ,
+    user: userSlice.reducer
+ 
   },
 });
 
