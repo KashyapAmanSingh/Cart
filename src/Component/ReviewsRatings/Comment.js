ff84fe5fb89be2b772668de45003ac31f16ef839
kashyapAmansingh <coder85113@gmail.com>
2023-10-01 10:25:54 +0530

    feature- Review and comment rating too

--- src/Component/ReviewsRatings/Comment.js
+++ src/Component/ReviewsRatings/Comment.js
@@ -1,20 +1,38 @@
 "use client";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Comment = () => {
   const [comment, setComment] = useState("");
+  const [fetchedUserId, setFetchedUserId] = useState("");
+
   const totalStar = useSelector((state) => state.Reviews.ratings);
   const ProductId = useSelector((state) => state.Reviews.ProductOrderId);
-  const userId = useSelector((state) => state.user.user);
-  console.log("Comment component    TotalStar:->  ", totalStar);
-  console.log("Comment component   ProductId ->  ", ProductId);
-  if (userId) {
-    console.log("User ID::-> ", userId._id);
-  } else {
-    console.log("User ID is null");
-  }
+  const userIdFromSlice = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    if (!userIdFromSlice) {
+      const fetchUserId = async () => {
+        try {
+          const response = await axios.get("/api/user");
+          setFetchedUserId(response.data);
+          console.log("Fetched user ", response);
+        } catch (error) {
+          console.error("Error fetching user ID:", error);
+        }
+      };
+
+      fetchUserId();
+    }
+  }, []); // userIdFromSlice  
+
+  console.log("Comment component TotalStar:-> ", totalStar);
+  console.log("Comment component ProductId -> ", ProductId);
+
+  const userId = fetchedUserId || userIdFromSlice;
+
+  console.log(userId ? "User ID::-> " + userId : "User ID is null");
 
   const handleCommentChange = (e) => {
     setComment(e.target.value);
@@ -24,7 +42,7 @@ const Comment = () => {
     const dataToSend = {
       rating: totalStar || 0,
       comment: comment || "",
-      userId: userId._id || "",
+      userId: userId || "", // Use userId here
       productId: ProductId || "",
     };
 
@@ -37,10 +55,10 @@ const Comment = () => {
         console.error("Error during request:", error);
       });
   };
+
   return (
     <div>
       <h2>Leave a Review</h2>
-
       <textarea
         rows="4"
         cols="50"
