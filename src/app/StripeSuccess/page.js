49e6cd60e995c5ab184963d7c6cb7ecfc8529a50
Kashyap_Aman_Singh <119684617+KashyapAmanSingh@users.noreply.github.com>
2023-10-08 23:37:40 +0530

    Feature- Ordered Item from backend

--- src/app/StripeSuccess/page.js
+++ src/app/StripeSuccess/page.js
@@ -1,55 +1,49 @@
-"use client";
-import { useSelector } from "react-redux";
+// http://localhost:3000/StripeSuccess?session_id=cs_test_b1qSsxX34jhdYKblUp2UHpveepfMJ4KEekvPVAy10DHztrmQurJq0FBVoX
 
-import Stripe from "stripe";
-import { useRouter } from "next/navigation";
-const stripe = new Stripe(
-  "sk_test_51Nr0qpSGcFt4Msz1nwiCDptTvHH171EgKDiBkfMv0wJz1hJYR8lO0a3Um69sdUo6M0kFGmhlyPF4mxp5ZmT1eFqw002qgRL5Ic"
-);
+"use client";
+import { fetchData } from "@/utils/FetchCode";
+import axios from "axios";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const SuccessPage = () => {
   const router = useRouter();
+  const params = useSearchParams();
+  const session_id = params.get("session_id");
+  const [state, setState] = useState();
+  
+  console.log(
+    state.data.retrievedSession.id,
+    "<<<<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>>>>____________________________________"
+  );
 
-  const session = useSelector((state) => state.cart.session);
+  useEffect(() => {
+    const fetchD = async () => {
+      try {
+        const response = await axios.post(`/api/retrieveSession`, {
+          id: session_id,
+        });
 
-  if (typeof window !== "undefined") {
-    console.log(session, "SuccessPage Payment SuccessPage");
+        setState(response.data);
+      } catch (error) {
+        console.error("Error retrieving data:", error);
+        // Handle the error, e.g., show an error message to the user
+      }
+    };
 
-    const storedSessionData = JSON.parse(localStorage.getItem("sessionData"));
-    if (storedSessionData) {
-      stripe.checkout.sessions.retrieve(
-        storedSessionData,
-        (err, retrievedSession) => {
-          if (err) {
-            console.error("Error retrieving session:", err);
-          } else {
-            console.log("Retrieved Session Details:", retrievedSession);
-
-            // Check if metadata exists and has order_id
-            const orderId = retrievedSession.metadata?.order_id;
-
-            if (orderId) {
-              console.log(orderId, "Order ID");
-            } else {
-              console.log("Order ID not found in session metadata.");
-            }
-          }
-        }
-      );
-    }
-  }
+    fetchD();
+  }, []);
 
   const redirectToHome = () => {
     console.log("Redirecting to Home Page");
-    // window.location.href = "/";
     router.push("/");
   };
 
   return (
     <div>
       <h1>Thank you for your payment!</h1>
-      <p>Your SuccessPageID: {session}</p>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <p>Your SuccessPageID: {session_id}</p>
       <button
         className="btn btn-danger"
         id="continueShoppingButton"
