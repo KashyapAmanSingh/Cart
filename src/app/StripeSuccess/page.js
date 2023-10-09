eb748eebb1b0c97242161f863d3cd7242691d184
kashyapAmansingh <coder85113@gmail.com>
2023-10-09 15:20:12 +0530

    Feature-Automatic Updating the order id of admin

--- src/app/StripeSuccess/page.js
+++ src/app/StripeSuccess/page.js
@@ -19,37 +19,65 @@ const SuccessPage = () => {
         const response = await axios.post(`/api/retrieveSession`, {
           id: session_id,
         });
-        // console.log(response);
+        // console.log("Response from API:", response.data);
         setStates(response.data);
       } catch (error) {
         console.error("Error retrieving data:", error);
       }
     };
+    
 
     fetchD();
   }, []);
+ 
+  if(state && state.data  ) { 
+    const {retrievedSession}=state.data;
 
-  // if (state && Object.keys(state).length > 0) {
-  //   console.log(
-  //     state.data,
-  //     "<<<<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>>>>____________________________________"
-  //   );
-  
-  //   if (state.data && state.data) {
-  //     for (const key in state.data) {
-  //       if (state.data.hasOwnProperty(key)) {
-  //         const value = state.data[key];
-  //         console.log(`${key}:`, value);
-  //         // You can do something with key and value here
-  //       }
-  //     }
-  //   } else {
-  //     console.log("retrieveSession is undefined");
-  // }
-  
+//     const {
+//        session_id: id,
+//        amount_subtotal: 1479700
+// ​
+// amount_total: 1751046
+// created: 1696751338
+// ​
+// currency: "inr"
 
-  
+// custom_text  
+      
+// customer_details: Object { email: "coder85113@gmail.com", name: "coder Aman Kashyap", phone: "+917992265306", … }
+      
+// invoice: "in_1NywMeSGcFt4Msz1Y3q8U7vK"
+      
 
+// line_items: Object { object: "list", has_more: false, 
+      
+// metadata: Object {  }
+// ​
+// mode: "payment"
+      
+      
+// payment_intent: "pi_3NywMYSGcFt4Msz11Fd4Jbkv"
+      
+// payment_method_types: Array [ "card" ]
+      
+// status: "complete"
+      
+      
+// total_details: Object { amount_discount: 0, amount_shipping: 5000, amount_tax: 266346 }
+// ​​
+// amount_discount: 0
+// ​​
+// amount_shipping: 5000
+// ​​
+// amount_tax: 266346
+      
+ 
+//     }=retrievedSession;
+
+
+
+  console.log("THIS IS STATE OD THE PAGE SUCCESS",retrievedSession);
+}
   return (
     <div>
       <h1>Thank you for your payment!</h1>
@@ -64,7 +92,7 @@ const SuccessPage = () => {
     </div>
   );
 };
-}
+// }
 export default SuccessPage;
 
 // source: "/api/.*",
--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,71 +1,86 @@
 import { NextResponse } from "next/server";
 import { Stripe } from "stripe";
-
-const stripe = new Stripe(
-  "whsec_58ecfad645057c11c0f34aaab2458334b729ee585a470cf8d19064c3cad85ed9",
-  {
-    apiVersion: "2020-08-27", //2023-08-16
-  }
-);
-const stripes = new Stripe(
-  "sk_test_51Nr0qpSGcFt4Msz1nwiCDptTvHH171EgKDiBkfMv0wJz1hJYR8lO0a3Um69sdUo6M0kFGmhlyPF4mxp5ZmT1eFqw002qgRL5Ic"
-);
-export async function POST(req, res) {
+import AdminOrder from "../../../../models/admin";
  
+// const endpointSecret =
+//   "whsec_58ecfad645057c11c0f34aaab2458334b729ee585a470cf8d19064c3cad85ed9";
+
+export async function POST(req, res) {
+  // const stripe = new Stripe(
+  //   "sk_test_51Nr0qpSGcFt4Msz1nwiCDptTvHH171EgKDiBkfMv0wJz1hJYR8lO0a3Um69sdUo6M0kFGmhlyPF4mxp5ZmT1eFqw002qgRL5Ic"
+  //   // { apiVersion: "2023-08-16" }
+  // );
+
+  // let event;
+
   try {
-    const sig = req.headers.get("stripe-signature"); // const sig = req.headers['stripe-signature'];
+    // const sig = req.headers.get("stripe-signature");
+ 
     const rawBody = await req.text();
 
-    const event = stripe.webhooks.constructEvent(
-      rawBody,
-      sig,
-      "sk_test_51Nr0qpSGcFt4Msz1nwiCDptTvHH171EgKDiBkfMv0wJz1hJYR8lO0a3Um69sdUo6M0kFGmhlyPF4mxp5ZmT1eFqw002qgRL5Ic"
-    );
+    const parsedBody = JSON.parse(rawBody);    
+    const eventType = parsedBody.type;
+ 
+    const paymentQuery = {};
 
-    console.log(
-      "Constructed event with type:%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
-      event.type
-    );
-
-    switch (event.type) {
+    switch (eventType) {
       case "checkout.session.async_payment_failed":
-        console.log(
-          " %%%%%%%%%%%%%%%%%%%%%%%%%%%%% event.type event.type",
-          event.data.object
-        );
+        paymentQuery.paymentStatus = "failed";
         break;
+
       case "checkout.session.async_payment_succeeded":
-        console.log(
-          " %%%%%%%%%%%%%%%%%%%%%%%%%%%%% event.type event.type",
-          event.data.object
-        );
-        break;
       case "checkout.session.completed":
-        console.log(
-          " %%%%%%%%%%%%%%%%%%%%%%%%%%%%% event.type event.type",
-          event.data.object
-        );
+        paymentQuery.paymentStatus = "paid";
         break;
+
       case "checkout.session.expired":
-        console.log(
-          " %%%%%%%%%%%%%%%%%%%%%%%%%%%%% event.type event.type",
-          event.data.object
-        );
+        paymentQuery.paymentStatus = "pending";
         break;
+
       default:
-        console.log(`Unhandled event type ${event.type}`);
+        paymentQuery.paymentStatus = "failed";
+
+        break;
     }
+
+    await AdminOrder.findOneAndUpdate(
+      { orderId: "evt_1NypULSGcFt4Msz1Bf4vPxw" },
+      {
+        $set: {
+          paymentStatus: paymentQuery.paymentStatus,
+          FinalPaymentStatus: eventType,
+        },
+      }
+    );
+
+    // Now you can use paymentQuery.paymentStatus for any additional logic or updates
+
+   
+
+    // event = await stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
+   
+
+    // switch (event.type) {
+    //   case "checkout.session.async_payment_failed":
+    //   case "checkout.session.async_payment_succeeded":
+    //   case "checkout.session.completed":
+    //   case "checkout.session.expired":
+    //     console.log("Handling event:----- --", event.type);
+    //     break;
+    //   default:
+    //     console.log("Unhandled event type:", event.type);
+    // }
 
     return NextResponse.json(
       { message: "Successfully received", rawBody },
       { status: 200 }
     );
   } catch (err) {
-    console.error(err);
+    console.error("Error handling webhook:", err);
 
     return NextResponse.error({
       message: "Webhook signature verification failed",
-      error: err.message, // Add more details about the error
+      error: err.message,
     });
   }
 }
