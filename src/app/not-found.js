923c88bb53d3b6d869bc1a8b9ee9c2ef267f5abd
Kashyap_Aman_Singh <119684617+KashyapAmanSingh@users.noreply.github.com>
2023-10-15 13:13:29 +0530

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <h1>This Page not Found</h1>
+      <h1>This Page not Found its natural to make mistake</h1>
       <Link href="/">
-        Go To Home 
+        Go To Home click here bro all is well its natural to make mistake
       </Link>
     </div>
   );
