c57ee68d436ea027327fc8cf44a1826c77683c06
kashyapAmansingh <coder85113@gmail.com>
2023-10-15 14:46:01 +0530

    Update- local env with kinde credentials & stripe too

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,8 +1,11 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(request, {params}) {
-	const endpoint = params.kindeAuth;
-	return handleAuth(request, endpoint);
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import parse from "url-parse";
+export async function GET(request, { params }) {
+	const parsedUrl = parse(request.url, true);
+  console.log(
+    "GET from the kind auth dynamic urls----------------------------------------------------------========================================================================",
+    request,"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",parsedUrl,parsedUrl.query, params);
+ 
+  const endpoint = params.kindeAuth;
+  return handleAuth(request, endpoint);
 }
-
-
