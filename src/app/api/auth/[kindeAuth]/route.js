f15ab7a223162da10dfa473be442768e5f1a5641
kashyapAmansingh <coder85113@gmail.com>
2023-10-15 16:43:19 +0530

    bug- Related to the order in dbs

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,10 +1,5 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import parse from "url-parse";
-export async function GET(request, { params }) {
-	const parsedUrl = parse(request.url, true);
-  console.log(
-    "GET from the kind auth dynamic urls----------------------------------------------------------========================================================================",
-    request,"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",parsedUrl,parsedUrl.query, params);
+ export async function GET(request, { params }) {
  
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
