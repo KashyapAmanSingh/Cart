157d4a118c707a590faf157c8ca2d8441568d0ee
Kashyap_Aman_Singh <119684617+KashyapAmanSingh@users.noreply.github.com>
2023-10-15 13:30:29 +0530

    Update- checks not found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <h1>This Page not Found  Plese refresh or go to home by click </h1>
+      <h1>This Page not Found  Plese refresh or go to Links that lead to a 404 page are often called broken or dead links and can be subject to link rot. </h1>
       <Link href="/">
         Go To Home click here bro all is well its natural to make mistake
       </Link>
