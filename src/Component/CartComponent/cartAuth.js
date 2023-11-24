d7b52da99ef476749296c453f884acb2f63ffa1d
kashyapAmansingh <coder85113@gmail.com>
2023-11-24 14:33:31 +0530

    bug- Fixed by updatig cart auth

--- src/Component/CartComponent/cartAuth.js
+++ src/Component/CartComponent/cartAuth.js
@@ -1,58 +1,23 @@
-"use client"
-import Cart from '@/Component/CartComponent/Cart';
-import React, { useEffect, useState } from 'react';
-import   Loader1  from '../Progress';
- import { useDispatch } from 'react-redux';
-import { addUser } from '@/redux/UserInfoSlice';
-import axios from 'axios';
-import SecureCartLogin from './secureCartLogin';
+"use client";
+import Cart from "@/Component/CartComponent/Cart";
+import React, { useEffect, useState } from "react";
+import Loader1 from "../Progress";
  
- 
+import SecureCartLogin from "./secureCartLogin";
+import SecurityAuth from "@/utils/SecurityAuth";
+
 const CartAuth = () => {
-  const [loading, setLoading] = useState(true);
-  const [user,   setData] = useState();
- 
-  const [authStatus, setAuthStatus] = useState(null);
-  const dispatch = useDispatch()
-  useEffect(() => {
-    const getKindeSession = async () => {
-      try {
-        const res = await fetch("/api/kindeSession");
-        const data = await res.json();
-        setAuthStatus(data.authenticated);
- console.log(data,data.authenticated,"💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃")
-        if (data.user?.id) {
-
-          const userResponse = await axios.get(`/api/user?id=${data.user.id}`);
-  const userData = userResponse.data; 
-
-  setData(userData.user);
-  dispatch(addUser(userData.user[0]));
-
-  console.log(userData.user[0]._id, "This is the best place to use user information");
-
-}    
-      } catch (error) {
-        console.error('Error fetching user information:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getKindeSession();
-  }, []);
+  const { isAuthenticated, isLoading } = SecurityAuth();
 
   return (
-    <div  >
-      {loading ? (
-     <Loader1/> 
-      ) : authStatus ? (
- 
+    <div>
+      {isLoading ? (
+        <Loader1 />
+      ) : isAuthenticated ? (
         <Cart />
- 
-      ) : ( 
-       <SecureCartLogin/>             //<Link></Link>
-       )}
+      ) : (
+        <SecureCartLogin /> //<Link></Link>
+      )}
     </div>
   );
 };
